c1902253c3c77f477c59903859ae6d3e4252dd9a
Jose Vicente <jantvb@gmail.com>
2021-09-24 17:41:24 -0400

    .java

--- android/src/main/java/com/dare/plugin/FaceMePlugin.java
+++ android/src/main/java/com/dare/plugin/FaceMePlugin.java
@@ -6,6 +6,22 @@ import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
+import android.content.Context;
+
+import com.cyberlink.faceme.FaceMeSdk;
+import com.cyberlink.faceme.LicenseManager;
+import com.cyberlink.faceme.FaceMeRecognizer;
+import com.cyberlink.faceme.RecognizerConfig;
+import com.cyberlink.faceme.DetectionModelSpeedLevel;
+import com.cyberlink.faceme.ExtractConfig;
+import com.cyberlink.faceme.ExtractionOption;
+import com.cyberlink.faceme.EnginePreference;
+import com.cyberlink.faceme.ExtractionModelSpeedLevel;
+import com.cyberlink.faceme.DetectionOutputOrder;
+import com.cyberlink.faceme.RecognizerMode;
+import com.cyberlink.faceme.DetectionSpeedLevel;
+import com.cyberlink.faceme.DetectionMode;
+
 @CapacitorPlugin(name = "FaceMe")
 public class FaceMePlugin extends Plugin {
 
@@ -18,5 +34,19 @@ public class FaceMePlugin extends Plugin {
         JSObject ret = new JSObject();
         ret.put("value", implementation.echo(value));
         call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void inizialize(String licenseKey) {
+        if (licenseKey != null) {
+            try {
+                FaceMeSdk.initialize(context.getApplicationContext(), licenseKey);
+                callback.onCallback(licenseKey);
+            } catch (Exception e) {
+                callback.error("Something went wrong. " + e);
+            }
+        } else {
+            callback.error("Please don't pass null values.");
+        }
     }
 }
