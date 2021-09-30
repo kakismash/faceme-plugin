99fca1b540325071775781c2c674ed7452723e8e
David Marrero <davidmarrero@ordyx.com>
2021-09-30 00:29:46 -0400

    Clean

--- src/web.ts
+++ src/web.ts
@@ -3,6 +3,7 @@ import { WebPlugin } from '@capacitor/core';
 import type { FaceMePlugin } from './definitions';
 
 export class FaceMeWeb extends WebPlugin implements FaceMePlugin {
+
   async echo(options: { value: string }): Promise<{ value: string }> {
     console.log(options.value);
     throw new Error('Method not implemented.');
@@ -13,17 +14,23 @@ export class FaceMeWeb extends WebPlugin implements FaceMePlugin {
     throw new Error('Method not implemented.');
   }
 
-  async enroll(options: { imageBase64: string }): Promise<{ collectionId: string }> {
+  async enroll(options: { imageBase64: string,
+                          name: string,
+                          data?: string }): Promise<{ collectionId: string }> {
     console.log(options.imageBase64);
     throw new Error('Method not implemented.');
   }
 
-  async search(options: { imageBase64: string }): Promise<{ collectionId: string }> {
+  async search(options: { imageBase64: string }): Promise<{ collectionId?: string,
+                                                            confidence?: number,
+                                                            name?: string,
+                                                            data?: string}> {
     console.log(options.imageBase64);
     throw new Error('Method not implemented.');
   }
 
-  async changeCollectionName(options: { collectionId: number, name: string }): Promise<{ value: string }> {
+  async setCollectionName(options: {collectionId: number,
+                                    name: string }): Promise<{ value: string }> {
     console.log(options.collectionId);
     console.log(options.name);
     throw new Error('Method not implemented.');
@@ -34,9 +41,20 @@ export class FaceMeWeb extends WebPlugin implements FaceMePlugin {
     throw new Error('Method not implemented.');
   }
 
-  async deleteFace(options: { faceId: number }): Promise<{ value: string }> {
-    console.log(options.faceId);
+  async setCollectionData(options: {collectionId: number,
+                                    data: string }): Promise<{ value: string }> {
+    console.log(options.collectionId);
+    console.log(options.data);
     throw new Error('Method not implemented.');
   }
 
+  async getCollectionData(options: { collectionId: number }): Promise<{ data: string }> {
+    console.log(options.collectionId);
+    throw new Error('Method not implemented.');
+  }
+
+  async deleteCollection(options: { collectionId: number }): Promise<{ value: string }> {
+    console.log(options.collectionId);
+    throw new Error('Method not implemented.');
+  }
 }
