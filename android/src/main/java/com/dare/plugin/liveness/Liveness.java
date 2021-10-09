07760a82bd445579565d05e8b826ee419d881b13
Jose Vicente <jantvb@gmail.com>
2021-10-09 11:17:10 -0400

    initCamera

--- android/src/main/java/com/dare/plugin/liveness/Liveness.java
+++ android/src/main/java/com/dare/plugin/liveness/Liveness.java
@@ -1,5 +1,23 @@
 package com.dare.plugin.liveness;
 
+import android.view.TextureView;
+import android.util.Size;
+
 public class Liveness {
+
+    private TextureView        mainCamPreviewView;
+    private AutoFitSurfaceView subCamPreviewView;
+    private StatListener       statListener;
+
+    private void initCamera() {
+        mainCamPreviewView = findViewById(R.id.mainCamPreviewView);
+        subCamPreviewView = findViewById(R.id.subCamPreviewView);
+
+        statListener = new StatListener();
+        cameraController = CameraFactory.create(this, mainCamPreviewView, subCamPreviewView, this, statListener, true);
+        cameraController.setUiSettings(uiSettings);
+        Size previewSize = uiSettings.getPreviewSize();
+        cameraController.setResolution(previewSize.getWidth(), previewSize.getHeight());
+    }
 
 }
