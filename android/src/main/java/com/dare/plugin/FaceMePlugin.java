76c18ba7e86e18cc7b15e37d72b6f9a18277d7b7
Jose Vicente <jantvb@gmail.com>
2021-10-28 21:20:41 -0400

    init camera - null argument

--- android/src/main/java/com/dare/plugin/FaceMePlugin.java
+++ android/src/main/java/com/dare/plugin/FaceMePlugin.java
@@ -1,14 +1,24 @@
 package com.dare.plugin;
 
 import android.Manifest;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Point;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.util.Base64;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Display;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import androidx.activity.result.ActivityResult;
 import androidx.core.content.FileProvider;
@@ -47,7 +57,7 @@ import java.util.List;
                 )
         }
 )
-public class FaceMePlugin extends Plugin {
+public class FaceMePlugin extends Plugin implements CameraActivity.CameraPreviewListener {
 
     private FaceMe implementation = new FaceMe();
 
@@ -60,6 +70,12 @@ public class FaceMePlugin extends Plugin {
     private static final String PERMISSION_DENIED_ERROR_PHOTOS = "User denied access to photos";
     private static final String NO_CAMERA_ERROR = "Device doesn't have a camera available";
     private static final String NO_CAMERA_ACTIVITY_ERROR = "Unable to resolve camera activity";
+
+    // keep track of previously specified orientation to support locking orientation:
+    private int previousOrientationRequest = -1;
+
+    private CameraActivity fragment;
+    private int containerViewId = 20;
 
     private CameraSettings settings = new CameraSettings();
 
@@ -176,11 +192,11 @@ public class FaceMePlugin extends Plugin {
             call.reject(NO_CAMERA_ERROR);
             return;
         }
-        settings = getSettings(call);
+        //settings = getSettings(call);
         openCamera(call);
     }
 
-    private CameraSettings getSettings(PluginCall call) {
+    /*private CameraSettings getSettings(PluginCall call) {
         CameraSettings settings = new CameraSettings();
         settings.setQuality(call.getInt("quality", CameraSettings.DEFAULT_QUALITY));
         settings.setWidth(call.getInt("width", 0));
@@ -193,17 +209,108 @@ public class FaceMePlugin extends Plugin {
             settings.setSource(CameraSource.PROMPT);
         }
         return settings;
-    }
+    }*/
 
     private void openCamera(final PluginCall call) {
-        if (checkCameraPermissions(call)) {
+        /*if (checkCameraPermissions(call)) {
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             if (takePictureIntent.resolveActivity(getContext().getPackageManager()) != null) {
                 startActivityForResult(call, takePictureIntent, "processCameraImage");
             } else {
                 call.reject(NO_CAMERA_ACTIVITY_ERROR);
             }
-        }
+        }*/
+        String position = "front";
+
+        final Integer x = call.getInt("x", 0);
+        final Integer y = call.getInt("y", 0);
+        final Integer width = call.getInt("width", 0);
+        final Integer height = call.getInt("height", 0);
+        final Integer paddingBottom = call.getInt("paddingBottom", 0);
+        final Boolean toBack = call.getBoolean("toBack", false);
+        final Boolean storeToFile = call.getBoolean("storeToFile", false);
+        final Boolean disableExifHeaderStripping = call.getBoolean("disableExifHeaderStripping", true);
+        final Boolean lockOrientation = call.getBoolean("lockAndroidOrientation", false);
+        previousOrientationRequest = getBridge().getActivity().getRequestedOrientation();
+
+        fragment = new CameraActivity();
+        fragment.setEventListener(this);
+        fragment.defaultCamera = position;
+        fragment.tapToTakePicture = false;
+        fragment.dragEnabled = false;
+        fragment.tapToFocus = true;
+        fragment.disableExifHeaderStripping = disableExifHeaderStripping;
+        fragment.storeToFile = storeToFile;
+        fragment.toBack = toBack;
+
+        bridge.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                DisplayMetrics metrics = getBridge().getActivity().getResources().getDisplayMetrics();
+                // lock orientation if specified in options:
+                if (lockOrientation) {
+                    getBridge().getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
+                }
+
+                // offset
+                int computedX = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, x, metrics);
+                int computedY = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, y, metrics);
+
+                // size
+                int computedWidth;
+                int computedHeight;
+                int computedPaddingBottom;
+
+                if(paddingBottom != 0) {
+                    computedPaddingBottom = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, paddingBottom, metrics);
+                } else {
+                    computedPaddingBottom = 0;
+                }
+
+                if(width != 0) {
+                    computedWidth = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, width, metrics);
+                } else {
+                    Display defaultDisplay = getBridge().getActivity().getWindowManager().getDefaultDisplay();
+                    final Point size = new Point();
+                    defaultDisplay.getSize(size);
+
+                    computedWidth = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, size.x, metrics);
+                }
+
+                if(height != 0) {
+                    computedHeight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, height, metrics) - computedPaddingBottom;
+                } else {
+                    Display defaultDisplay = getBridge().getActivity().getWindowManager().getDefaultDisplay();
+                    final Point size = new Point();
+                    defaultDisplay.getSize(size);
+
+                    computedHeight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, size.y, metrics) - computedPaddingBottom;
+                }
+
+                fragment.setRect(computedX, computedY, computedWidth, computedHeight);
+
+                FrameLayout containerView = getBridge().getActivity().findViewById(containerViewId);
+                if(containerView == null){
+                    containerView = new FrameLayout(getActivity().getApplicationContext());
+                    containerView.setId(containerViewId);
+
+                    getBridge().getWebView().setBackgroundColor(Color.TRANSPARENT);
+                    ((ViewGroup)getBridge().getWebView().getParent()).addView(containerView);
+                    if(toBack == true) {
+                        getBridge().getWebView().getParent().bringChildToFront(getBridge().getWebView());
+                    }
+
+                    FragmentManager fragmentManager = getBridge().getActivity().getFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.add(containerView.getId(), fragment);
+                    fragmentTransaction.commit();
+
+                    call.success();
+                } else {
+                    call.reject("camera already started");
+                }
+            }
+        });
     }
 
     private boolean checkCameraPermissions(PluginCall call) {
@@ -230,10 +337,84 @@ public class FaceMePlugin extends Plugin {
         }
     }
 
-    @ActivityCallback
+    /*@ActivityCallback
     public void processCameraImage(PluginCall call, ActivityResult result) {
         settings = getSettings(call);
 
+    }*/
+
+    @Override
+    protected void handleOnResume() {
+        super.handleOnResume();
+    }
+
+    @Override
+    public void onPictureTaken(String originalPicture) {
+        JSObject jsObject = new JSObject();
+        jsObject.put("value", originalPicture);
+        getSavedCall().success(jsObject);
+    }
+
+    @Override
+    public void onPictureTakenError(String message) {
+        getSavedCall().reject(message);
+    }
+
+    @Override
+    public void onSnapshotTaken(String originalPicture) {
+        JSObject jsObject = new JSObject();
+        jsObject.put("value", originalPicture);
+        getSavedCall().success(jsObject);
+    }
+
+    @Override
+    public void onSnapshotTakenError(String message) {
+        getSavedCall().reject(message);
+    }
+
+    @Override
+    public void onFocusSet(int pointX, int pointY) {
+
+    }
+
+    @Override
+    public void onFocusSetError(String message) {
+
+    }
+
+    @Override
+    public void onBackButton() {
+
+    }
+
+    @Override
+    public void onCameraStarted() {
+        PluginCall pluginCall = getSavedCall();
+        System.out.println("camera started");
+        pluginCall.success();
+    }
+
+    @Override
+    public void onStartRecordVideo() {
+
+    }
+
+    @Override
+    public void onStartRecordVideoError(String message) {
+        getSavedCall().reject(message);
+    }
+
+    @Override
+    public void onStopRecordVideo(String file) {
+        PluginCall pluginCall = getSavedCall();
+        JSObject jsObject = new JSObject();
+        jsObject.put("videoFilePath", file);
+        pluginCall.success(jsObject);
+    }
+
+    @Override
+    public void onStopRecordVideoError(String error) {
+        getSavedCall().reject(error);
     }
 
 
